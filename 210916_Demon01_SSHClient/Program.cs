5d80a822402460d4ee635fbe953a6734e7e50728
albertzhao <szdxzhy@outlook.com>
2021-09-17 10:44:20 +0800

    vertify.

--- 210916_Demon01_SSHClient/Program.cs
+++ 210916_Demon01_SSHClient/Program.cs
@@ -15,7 +15,7 @@ namespace _210916_Demon01_SSHClient
         {
             byte[] expectedFingerPrint = new byte[] {
                                             0x66, 0x31, 0xaf, 0x00, 0x54, 0xb9, 0x87, 0x31,
-                                            0xff, 0x58, 0x1c, 0x31, 0xb1, 0xa2, 0x4c, 0x6b
+                                            0xff, 0x58, 0x1c, 0x31, 0xb1, 0xa2, 0x4c, 0x6b,
                                         };
 
             using (var client = new SshClient("sftp.foo.com",8080, "guest", "pwd"))
