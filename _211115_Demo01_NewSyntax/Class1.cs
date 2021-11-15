6102e2ec0b7519cf0376d7d3efa0bad43a141ccf
albertzhao <szdxzhy@outlook.com>
2021-11-15 16:39:50 +0800

    C#新语法

---
+++ _211115_Demo01_NewSyntax/Class1.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _211115_Demo01_NewSyntax
+{
+    internal class Class1:IDisposable
+    {
+        Article article1 = new Article();
+
+        public void Dispose()
+        {
+            Console.WriteLine("资源被释放啦");
+        }
+    }
+}
---
+++ _211115_Demo01_NewSyntax/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _211115_Demo01_NewSyntax;
+
+Console.WriteLine("Hello, World!");
+Article article = new Article();
+TestUSINGImplement();
+Console.WriteLine("Albert");
+
+static void TestUSINGImplement()
+{
+    using Class1 class1 = new Class1();
+}
