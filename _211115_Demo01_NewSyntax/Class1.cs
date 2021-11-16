e66d43a5ba28a6f0dba3c9a8b05a5e35f89842f0
albertzhao <szdxzhy@outlook.com>
2021-11-16 15:23:08 +0800

    .NET6新增语法：全局using,using资源,record类型,可空引用类型,副本拷贝,文件范围的命名空间声明”

--- _211115_Demo01_NewSyntax/Class1.cs
+++ _211115_Demo01_NewSyntax/Class1.cs
@@ -1,13 +1,12 @@
 ﻿
-namespace _211115_Demo01_NewSyntax
+namespace _211115_Demo01_NewSyntax;
+internal class Class1 : IDisposable
 {
-    internal class Class1:IDisposable
-    {
-        Article article1 = new Article();
+    Article article1 = new Article();
 
-        public void Dispose()
-        {
-            Console.WriteLine("资源被释放啦");
-        }
+    public void Dispose()
+    {
+        Console.WriteLine("资源被释放啦");
     }
 }
+
--- _211115_Demo01_NewSyntax/Program.cs
+++ _211115_Demo01_NewSyntax/Program.cs
@@ -1,12 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _211115_Demo01_NewSyntax;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("HelloAlbert");
 Article article = new Article();
 TestUSINGImplement();
 Console.WriteLine("Albert");
 
+
+//注意USING资源管理陷阱
+{
+    using var fs = File.OpenWrite("D:/1.txt");
+    using var ws = new StreamWriter(fs);
+    ws.WriteLine("Albert is very elegant.");
+}
+string s = File.ReadAllText("D:/1.txt");
+Console.WriteLine(s);
+
+Student student = new Student(1,"albert",24);
+Student student2 = new Student(1, "albert", 24);
+Console.WriteLine(student); //打印出学生的所有信息
+Console.WriteLine(student==student2);// True，比对各自含有的信息是否相同
+
+//拷贝的便捷方式with
+Student student3 = student;
+Console.WriteLine(Object.ReferenceEquals(student,student3));//True
+
+
+Student student4 = student with { };
+Console.WriteLine(student4);//Student { id = 1, name = albert, age = 24, height = 0 }
+Console.WriteLine(Object.ReferenceEquals(student,student4));//False
+
 static void TestUSINGImplement()
 {
-    using Class1 class1 = new Class1();
+    using Class1 class1 = new Class1();//在作用域内执行完毕，资源被释放
 }
+
+public record Student(int id,string name,int age)
+{
+    public int height { get; set; }
+}
+
