15e8c9e665bdf847fc25288309198542988a39db
albertzhao <szdxzhy@outlook.com>
2021-11-18 16:38:45 +0800

    增加学习Go的C#实现

---
+++ _211118_Demo01_GolangCSharpImplement/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//C# 字典排序
+Dictionary<string, int> dicStuManager = new Dictionary<string, int>();
+dicStuManager.Add("AlbertZhao", 15);
+dicStuManager.Add("AlbertLi", 14);
+dicStuManager.Add("AlbertChen", 13);
+dicStuManager.Add("AlbertHuang", 12);
+
+var tempdic = dicStuManager.OrderBy(o => o.Key);//降序排序
+
+foreach (var item in tempdic)
+{
+    System.Console.WriteLine(item.Key);
+}
+
+//写一个程序，统计一个字符串中每个单词出现的次数。比如：”how do you do”中how=1 do=2 you=1。
+Dictionary<string,int> dicWords = new Dictionary<string,int>();
+var words = "how do you do";
+var splictWords = words.Split(' ');
+foreach (var word in splictWords)
+{
+    if (dicWords.ContainsKey(word))
+    {
+        dicWords[word]++;
+    }
+    else
+    {
+        dicWords.Add(word, 1);
+    }
+}
+
+foreach (var item in dicWords)
+{
+    Console.WriteLine(item.Key+":"+item.Value);
+}
