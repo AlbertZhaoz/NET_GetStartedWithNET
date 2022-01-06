870086c4ef20015ec2a943eea085b8e3433448c8
albertzhao <szdxzhy@outlook.com>
2022-01-06 15:18:01 +0800

    Add 多线程拷贝目录

--- _220115_DownloadFromRemote/Program.cs
+++ _220115_DownloadFromRemote/Program.cs
@@ -1,12 +1,25 @@
-﻿var remoteFileDir = @"\\Redmond\exchange\Build\SUBSTRATE\15.20.4870.000\Target\dev\data\Microsoft.Exchange.Data.NetCore\retail\amd64\";
+﻿
+var projectReference = @"$(Inetroot)\sources\dev\common\src\Diagnostics.NetCore\Microsoft.Exchange.Diagnostics.NetCore.csproj";
+var dirNames = projectReference.Split('\\');
+var projDir = dirNames.First(e => e.Contains(".NetCore") || e.Contains(".NetStd"));
+Console.WriteLine(projDir);
+
+
+
+var remoteFileDir = @"\\Redmond\exchange\Build\SUBSTRATE\15.20.4870.000\Target\dev\data\Microsoft.Exchange.Data.NetCore\retail\amd64\";
 var localFileDir = @"F:\TestDownload";
 DateTime dt = DateTime.Now;
-await CopyDirectory(remoteFileDir, localFileDir);
+
+
+var tasks = new List<Task>();
+CopyDirectory(remoteFileDir, localFileDir, tasks);
+Task.WhenAll(tasks).Wait();
+
 DateTime dt2 = DateTime.Now;
-Console.WriteLine($"Down suceessfully.{dt2-dt}");
+Console.WriteLine($"Down suceessfully.{dt2 - dt}");
 
 
-static async Task CopyDirectory(string sourcePath, string destPath)
+static void CopyDirectory(string sourcePath, string destPath, List<Task> tasks)
 {
     string floderName = Path.GetFileName(sourcePath);
     DirectoryInfo di = Directory.CreateDirectory(Path.Combine(destPath, floderName));
@@ -16,18 +29,13 @@ static async Task CopyDirectory(string sourcePath, string destPath)
     {
         if (Directory.Exists(file))
         {
-            await Task.Run(() => CopyDirectory(file, di.FullName));
+            CopyDirectory(file, di.FullName, tasks);
         }
         else
         {
-            //using (var input = File.OpenRead(file))
-            //{
-            //    using (var output = Create(Path.Combine(di.FullName, Path.GetFileName(file))))
-            //    {
-            //        await Task.Run(()=>input.CopyToAsync(output));
-            //    }
-            //}
-            await Task.Run(() => File.Copy(file, Path.Combine(di.FullName, Path.GetFileName(file)), true));
+
+            var task = Task.Run(() => File.Copy(file, Path.Combine(di.FullName, Path.GetFileName(file)), true));
+            tasks.Add(task);
             //File.Copy(file, Path.Combine(di.FullName, Path.GetFileName(file)), true);
         }
     }
